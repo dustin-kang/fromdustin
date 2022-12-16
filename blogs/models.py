86146a8c63ed8ff15ac0653b9e79f0ef8d5fbabf
Dongwoo Kang <iamdustin@naver.com>
2022-12-16 17:51:15 +0900

    [#10] Addig author Field

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
@@ -7,13 +8,14 @@ class Post(models.Model):
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
-    # author : 추후 작성 예정
     
     head_image = models.ImageField(upload_to='blogs/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blogs/files/%Y/%m/%d/', blank=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
