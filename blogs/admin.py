64765b1efef11fde96aedd04b19bc26aef80aebf
Dongwoo Kang <iamdustin@naver.com>
2022-12-17 15:45:32 +0900

    [#12] Add category

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Category
 
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )} # name을 입력했을 때, 자동으로 slug가 만들어진다.
+
+admin.site.register(Category, CategoryAdmin)
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,20 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+    def get_context_data(self, **kwargs):
+      context = super(PostList,self).get_context_data() # 기존 제공한 기능 context에 저장
+      context['categories'] = Category.objects.all() # 카테고리 모든 정보를 딕셔너리 키와 연결
+      context['no_category_post_count'] = Post.objects.filter(category=None).count() # 카테고리가 지정되지 않은 포스트 수 정보
+      return context
+
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self, **kwargs):
+      context = super(PostDetail,self).get_context_data() # 기존 제공한 기능 context에 저장
+      context['categories'] = Category.objects.all() # 카테고리 모든 정보를 딕셔너리 키와 연결
+      context['no_category_post_count'] = Post.objects.filter(category=None).count() # 카테고리가 지정되지 않은 포스트 수 정보
+      return context
